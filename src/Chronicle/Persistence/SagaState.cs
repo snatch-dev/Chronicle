bce124962f1f10043ea902dd7a330e25989ecd89
dima <dima.zhemkov@gmail.com>
2020-10-19 22:43:01 +0300

    Add Revision property to ISagaState (#53)

--- src/Chronicle/Persistence/SagaState.cs
+++ src/Chronicle/Persistence/SagaState.cs
@@ -8,12 +8,13 @@ namespace Chronicle.Persistence
         public Type Type { get; }
         public SagaStates State { get; private set; }
         public object Data { get; private set; }
+        public uint Revision { get; }
 
-        private SagaState(SagaId id, Type type, SagaStates state, object data) 
-            => (Id, Type, State, Data) = (id, type, state, data);
+        private SagaState(SagaId id, Type type, SagaStates state, uint revision, object data) 
+            => (Id, Type, State, Revision, Data) = (id, type, state, revision, data);
 
-        public static ISagaState Create(SagaId id, Type type, SagaStates state, object data = null) 
-            => new SagaState(id, type, state, data);
+        public static ISagaState Create(SagaId id, Type type, SagaStates state, uint revision, object data = null) 
+            => new SagaState(id, type, state, revision, data);
 
         public void Update(SagaStates state, object data = null)
         {
